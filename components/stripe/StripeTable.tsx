de6fd9c4a9220156031a59ed4b99a3f58ffff2bc
lucasrodrigues <lucasrodrigues@loop4.com.br>
2024-08-12 21:25:39 -0300

    feat: .env para o stripe-pricing-table

--- components/stripe/StripeTable.tsx
+++ components/stripe/StripeTable.tsx
@@ -1,27 +1,31 @@
-'use client'
-import { User } from '@supabase/supabase-js';
-import React, { useEffect } from 'react';
+"use client";
+import { User } from "@supabase/supabase-js";
+import React, { useEffect } from "react";
 
-interface StripePricingTableProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> {
-  'pricing-table-id': string;
-  'publishable-key': string;
+interface StripePricingTableProps
+  extends React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLElement>,
+    HTMLElement
+  > {
+  "pricing-table-id": string;
+  "publishable-key": string;
 }
 
 declare global {
   namespace JSX {
     interface IntrinsicElements {
-      'stripe-pricing-table': StripePricingTableProps;
+      "stripe-pricing-table": StripePricingTableProps;
     }
   }
 }
 
 type Props = {
   user: User;
-}
+};
 
 const StripePricingTable = ({ user }: Props) => {
   useEffect(() => {
-    const script = document.createElement('script');
+    const script = document.createElement("script");
     script.src = "https://js.stripe.com/v3/pricing-table.js";
     script.async = true;
 
@@ -29,20 +33,22 @@ const StripePricingTable = ({ user }: Props) => {
 
     return () => {
       document.body.removeChild(script);
-    }
+    };
   }, []);
 
+  const stripePricingTable = process.env.STRIPE_PRICING_TABLE_ID as string;
+  const stripePublishableKey = process.env.STRIPE_PUBLISHABLE_KEY as string;
+
   return (
-    <div className='flex flex-1 flex-col w-full'>
+    <div className="flex flex-1 flex-col w-full">
       <stripe-pricing-table
-          pricing-table-id="prctbl_1PjPfqGJGCWWTgK5XVY92DGt"
-          publishable-key="pk_live_51NHSIeGJGCWWTgK5oClYlB3rLqfTqPEKQyJGR7mBUPCgc7JPxi3Fynw0tLox8ItnSSq9DoaAYks03ig4uSMyHwoo009Y8UIis5"
-          client-reference-id={user.id}
-          customer-email={user.email}
-      >
-      </stripe-pricing-table>
+        pricing-table-id={stripePricingTable}
+        publishable-key={stripePublishableKey}
+        client-reference-id={user.id}
+        customer-email={user.email}
+      ></stripe-pricing-table>
     </div>
   );
-}
+};
 
 export default StripePricingTable;
